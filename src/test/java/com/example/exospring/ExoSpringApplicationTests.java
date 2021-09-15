ac63ce6d07650e96e846b8e22b5fd53c09472afe
Navezm <navez.martin@gmail.com>
2021-09-15 15:11:08 +0200

    Start project

---
+++ src/test/java/com/example/exospring/ExoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
